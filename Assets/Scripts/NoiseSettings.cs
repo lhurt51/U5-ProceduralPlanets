e7cc4596f9b695cfbd179b52193d8239921b0238
Liam Hurt <liamhurt51@gmail.com>
2018-08-31 19:34:29 -0700

    Adding noise generation to the world that i have build. it all lines up seamlessly and works well with the semi LOD system

---
+++ Assets/Scripts/NoiseSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings {
+
+    [Range(1, 8)]
+    public int numLayers = 1;
+    public float strength = 1.0f;
+    public float baseRoughness = 1.0f;
+    public float roughness = 2.0f;
+    public float persistance = 0.5f;
+    public float minVal;
+    public Vector3 center;
+
+}
